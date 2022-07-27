072f0488a6da2ea961959860d4c3395fb09d9b52
pantkdkrt <p.engineering6415@gmail.com>
2022-07-27 10:49:50 +0700

    Authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
     def create
       @user = User.find_by(email: params[:user][:email].downcase)
       if @user
-        if @user.unconfirmed?
-          redirect_to new_confirmation_path, alert: "Incorrect email or password."
-        elsif @user.authenticate(params[:user][:password])
+        #if @user.unconfirmed?
+          #redirect_to new_confirmation_path, alert: "Incorrect email or password."
+        if @user.authenticate(params[:user][:password])
           login @user
-          redirect_to root_path, notice: "Signed in."
+          redirect_to inventory_path, notice: "Signed in."
         else
           flash.now[:alert] = "Incorrect email or password."
           render :new, status: :unprocessable_entity
@@ -23,7 +23,7 @@ class SessionsController < ApplicationController
   
     def destroy
       logout
-      redirect_to root_path, notice: "Signed out."
+      redirect_to login_path, notice: "Signed out."
     end
   
     def new
